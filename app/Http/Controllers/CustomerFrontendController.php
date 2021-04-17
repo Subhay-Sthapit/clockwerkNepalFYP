00da6b3a3e321a808299674e04261f0eba1b0d43
subhay999 <68510957+Subhay-Sthapit@users.noreply.github.com>
2021-04-17 12:47:59 +0545

    user home page made more dynamic

--- app/Http/Controllers/CustomerFrontendController.php
+++ app/Http/Controllers/CustomerFrontendController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\customer;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CustomerFrontendController extends Controller
 {
     //
 
-    public function user_home(User $user, customer $customer){
-        return view('user-home',['user'=>$user,'customer'=>$customer]);
+    public function user_home(customer $customer){
+        $users = DB::table('users')->get();
+        $service_centers = DB::table('service_centers')->get();
+        return view('user-home',['customer'=>$customer,'service_centers'=>$service_centers,'users'=>$users]);
     }
 
     public function user_profile(customer $customer){
